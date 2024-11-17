900c3974158dd199341285a8a541aaa2db5723a4
ttripathi167 <86569777+ttripathi167@users.noreply.github.com>
2024-11-16 23:17:28 -0500

    Update jquery.fittext.js

--- js/jquery.fittext.js
+++ js/jquery.fittext.js
@@ -2,9 +2,9 @@
 /*!
 * FitText.js 1.2
 *
-* Copyright 2011, Dave Rupert http://daverupert.com
+* Copyright 2024, Vaishnavi
 * Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
+* 
 *
 * Date: Thu May 05 14:23:00 2011 -0600
 */
